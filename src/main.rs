38ca2b43445f0fb09ad91fa0aa399fa1dd65b0da
Matthew White <mwhite@plex.com>
2022-01-15 00:09:48 -0500

    Make for loop a little more functional

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod game;
 use game::fizzbuzz::FizzBuzz;
 
 fn main() {
-    for i in 1..=100 {
-        println!("{}", FizzBuzz::from(i));
+    for y in (1..=100).into_iter().map(|x| FizzBuzz::from(x)) {
+        println!("{}", y);
     }
 }
