d182dae28d04a43e25250400cf66cb9c79ddaf5d
Matthew White <mwhite@plex.com>
2022-01-14 21:39:56 -0500

    Imperative loop implementation of fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut i = 1;
+
+    while i <= 100 {
+        if i % 5 == 0 && i % 3 == 0 {
+            println!("FizzBuzz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else {
+            println!("{}", i);
+        }
+
+        i += 1;
+    }
 }
