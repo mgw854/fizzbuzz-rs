7cb0893cb0d29d6d6065f22cd6d6f795ed610b5d
Matthew White <mwhite@plex.com>
2022-01-14 21:45:20 -0500

    Extracted logic into a function

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ fn main() {
     let mut i = 1;
 
     while i <= 100 {
-        if i % 5 == 0 && i % 3 == 0 {
-            println!("FizzBuzz");
-        } else if i % 5 == 0 {
-            println!("Buzz");
-        } else if i % 3 == 0 {
-            println!("Fizz");
-        } else {
-            println!("{}", i);
-        }
+        println!("{}", fizzbuzz(i));
 
         i += 1;
+    }
+}
+
+fn fizzbuzz(i: u32) -> String {
+    if i % 5 == 0 && i % 3 == 0 {
+        "FizzBuzz".to_string()
+    } else if i % 5 == 0 {
+        "Buzz".to_string()
+    } else if i % 3 == 0 {
+        "Fizz".to_string()
+    } else {
+        i.to_string()
     }
 }
