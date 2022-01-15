de0ceca891c1cc46522c327fdb7db34c4796c6f9
Matthew White <mwhite@plex.com>
2022-01-14 21:50:22 -0500

    Pattern matching

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,10 @@ fn main() {
 }
 
 fn fizzbuzz(i: u32) -> String {
-    if i % 5 == 0 && i % 3 == 0 {
-        "FizzBuzz".to_string()
-    } else if i % 5 == 0 {
-        "Buzz".to_string()
-    } else if i % 3 == 0 {
-        "Fizz".to_string()
-    } else {
-        i.to_string()
+    match (i % 3, i % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        _ => i.to_string(),
     }
 }
