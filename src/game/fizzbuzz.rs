2e5cf1a8913faa16d04a1cae0bd979520e9dbc5a
Matthew White <mwhite@plex.com>
2022-01-15 00:39:47 -0500

    Make FizzBuzz generic over T : Unsigned + Integer

--- src/game/fizzbuzz.rs
+++ src/game/fizzbuzz.rs
@@ -1,14 +1,22 @@
 use std::fmt::Display;
 
+use num::{FromPrimitive, Integer, Unsigned};
+
 #[derive(PartialEq, Eq, Debug)]
-pub enum FizzBuzz {
+pub enum FizzBuzz<T>
+where
+    T: Unsigned + Integer,
+{
     FizzBuzz,
     Fizz,
     Buzz,
-    Number { x: u32 },
+    Number { x: T },
 }
 
-impl Display for FizzBuzz {
+impl<T> Display for FizzBuzz<T>
+where
+    T: Unsigned + Integer + Display,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
             FizzBuzz::FizzBuzz => f.write_str("FizzBuzz"),
@@ -19,13 +27,19 @@ impl Display for FizzBuzz {
     }
 }
 
-impl From<u32> for FizzBuzz {
-    fn from(i: u32) -> Self {
-        match (i % 3, i % 5) {
-            (0, 0) => FizzBuzz::FizzBuzz,
-            (0, _) => FizzBuzz::Fizz,
-            (_, 0) => FizzBuzz::Buzz,
-            _ => FizzBuzz::Number { x: i },
+impl<T> From<T> for FizzBuzz<T>
+where
+    T: Unsigned + Integer + FromPrimitive + Copy,
+{
+    fn from(i: T) -> Self {
+        if i % T::from_u32(15).unwrap() == T::zero() {
+            FizzBuzz::FizzBuzz
+        } else if i % T::from_u32(5).unwrap() == T::zero() {
+            FizzBuzz::Buzz
+        } else if i % T::from_u32(3).unwrap() == T::zero() {
+            FizzBuzz::Fizz
+        } else {
+            FizzBuzz::Number { x: i }
         }
     }
 }
@@ -36,41 +50,41 @@ mod test {
 
     #[test]
     fn from_1_number() {
-        assert_eq!(FizzBuzz::Number { x: 1 }, FizzBuzz::from(1))
+        assert_eq!(FizzBuzz::Number { x: 1 as u32 }, FizzBuzz::from(1))
     }
 
     #[test]
     fn from_3_number() {
-        assert_eq!(FizzBuzz::Fizz, FizzBuzz::from(3))
+        assert_eq!(FizzBuzz::Fizz, FizzBuzz::from(3 as u32))
     }
 
     #[test]
     fn from_5_number() {
-        assert_eq!(FizzBuzz::Buzz, FizzBuzz::from(5))
+        assert_eq!(FizzBuzz::Buzz, FizzBuzz::from(5 as u32))
     }
 
     #[test]
     fn from_15_number() {
-        assert_eq!(FizzBuzz::FizzBuzz, FizzBuzz::from(15))
+        assert_eq!(FizzBuzz::FizzBuzz, FizzBuzz::from(15 as u32))
     }
 
     #[test]
     fn display_1_number() {
-        assert_eq!("1", FizzBuzz::Number { x: 1 }.to_string())
+        assert_eq!("1", FizzBuzz::Number { x: 1 as u32 }.to_string())
     }
 
     #[test]
     fn display_3_number() {
-        assert_eq!("Fizz", FizzBuzz::Fizz.to_string())
+        assert_eq!("Fizz", FizzBuzz::<u32>::Fizz.to_string())
     }
 
     #[test]
     fn display_5_number() {
-        assert_eq!("Buzz", FizzBuzz::Buzz.to_string())
+        assert_eq!("Buzz", FizzBuzz::<u32>::Buzz.to_string())
     }
 
     #[test]
     fn display_15_number() {
-        assert_eq!("FizzBuzz", FizzBuzz::FizzBuzz.to_string())
+        assert_eq!("FizzBuzz", FizzBuzz::<u32>::FizzBuzz.to_string())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod game;
 use game::fizzbuzz::FizzBuzz;
 
 fn main() {
-    for y in (1..=100).into_iter().map(|x| FizzBuzz::from(x)) {
+    for y in (1 as u32..=100).into_iter().map(|x| FizzBuzz::from(x)) {
         println!("{}", y);
     }
 }
